a8a745616202bb39a912aa157e7fdbd30f2a5033
Marllon Freitas <marllonfreitas64@gmail.com>
2023-07-06 21:55:43 -0300

    Fifth class

---
+++ C# Code Annotations - 05/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Rectangle : Shape
+    {
+        public double Lenght { get; set; }  
+
+        public double Width { get; set; }   
+
+        public Rectangle(double lenght, double width)
+        {
+            Name = "Rectangle";
+            Lenght = lenght;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Lenght * Width;
+        }
+
+        // You can replace the method using override
+        public override void GetInfo()
+        {
+            //execute the base class GetInfo
+            base.GetInfo();
+            Console.WriteLine($"It has a Length of {Lenght} and a Width of {Width}");
+        }
+    }
+}
